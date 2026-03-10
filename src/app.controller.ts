97b23a7cb0e7503121d22da8bffecd591564346a
dmsyddl <shineeol20@gmail.com>
2026-03-10 14:28:40 +0900

    feat: 네이버 OpenAPI 조회 테스트

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('search')
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get('search')
-  async getTest(@Query('name') name: string) {
+  
+  @Get()
+  async getCafeInfo(@Query('name') name: string) {
     return await this.appService.cafeSearch(name);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -11,25 +11,21 @@ export class AppService {
     ) {}
 
     async cafeSearch(keyword: string) {
-        console.log('네이버 API 검색 시작');
+        const NAVER_API_URL = this.configService.get<string>('NAVER_API_URL');
+        const NAVER_CLIENT_ID = this.configService.get<string>('NAVER_CLIENT_ID');
+        const NAVER_SECRET_ID = this.configService.get<string>('NAVER_SECRET_ID');
 
-        const url = 'https://openapi.naver.com/v1/search/local.json';
-        const cilentId = this.configService.get<string>('NAVER_CLIENT_ID');
-        const secretId = this.configService.get<string>('NAVER_SECRET_ID');
-
+        const url = NAVER_API_URL + '/v1/search/local.json';
         const response = await firstValueFrom(
             this.httpService.get(url, {
-                params: { query: keyword, display: 100},
+                params: { query: keyword, display: 5},
                 headers: {
-                    'X-Naver-Client-Id': cilentId,
-                    'X-Naver-Client-Secret': secretId,
+                    'X-Naver-Client-Id': NAVER_CLIENT_ID,
+                    'X-Naver-Client-Secret': NAVER_SECRET_ID,
                 }
             })
         )
 
-        const cafeName = response.data.items[0].title;
-        const cafeAddress = response.data.items[0].address;
-
-        return {cafeName, cafeAddress};
+        return response.data;
     }
 }
